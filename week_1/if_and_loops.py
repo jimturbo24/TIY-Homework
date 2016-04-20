89aff75daab1b318b412636e943555c7f546d93e
James Trbovich <jim.trbovich@gmail.com>
2016-04-20 15:13:51 -0500

    impoved the function of 'if_andloops' and created python functions within 'pigLatin' to handle word and sentance translation. Integrated pig latin functions into a short script allowing user input

--- week_1/if_and_loops.py
+++ week_1/if_and_loops.py
@@ -5,25 +5,25 @@ ageList = []
 choice = "defaulty"
 
 while "y" in choice.lower():
-    name = input("Enter first and last name: ")
-    if not name or len(name.split()) < 2:
-        print("Invalid input, please start again")
-        continue
-    color = input("Enter a favorite color: ")
-    if not color:
-        print("Invalid input, please start again")
-        continue
-    age = input("Enter an age: ")
-    if not age or not age.isdigit():
-        print("Invalid input, please start again")
-        continue
+
+    name = color = age = ""
+
+    while not name or len(name.split()) < 2:
+        name = input("Enter first and last name: ")
+
+    while not color:
+        color = input("Enter a favorite color: ")
+
+    while not age or not age.isdigit():
+        age = input("Enter an age: ")
+
     ageList.append(age)
     nameList.append(name)
     colorList.append(color)
 
     choice = input("Would you like to enter another name, color, and age (y/n)?")
-    if "y" in choice.lower():
-        continue
+    #if "y" in choice.lower():
+    #    continue
 
 while nameList:
     print("Name:" + nameList.pop() + " Color:" + colorList.pop() + " Age:" + ageList.pop(), end=' --- ')
--- week_1/pigLatin.py
+++ week_1/pigLatin.py
@@ -1,20 +1,32 @@
-choice = "defalty"
-
-print("This program translates words into pig latin.")
-
-while "y" in choice.lower():
-    word = input("Please enter a word: ")
-
+def pigLatin(word):
     if word[0] in "aeiou":
         wordTrunc = word[1:]
-        print(word + " in pig latin is " + wordTrunc + "say")
+        return wordTrunc + "say"
     else:
         firstLetter = word[0]
         wordTrunc = word[1:]
-        print(word + " in pig latin is " + wordTrunc + firstLetter + "ay")
+        return wordTrunc + firstLetter + "ay"
 
-    choice = input("Would you like to enter another word (y/n)? ")
-    if "y" in choice.lower():
-        continue
+def pigTranslate(sentance):
+    for word in sentance.split():
+        if "," in word:
+            noPunc = word[0:(len(word)-1)]
+            print(pigLatin(noPunc) + ",", end=" ")
+        elif "." in word:
+            noPunc = word[0:(len(word)-1)]
+            print(pigLatin(noPunc) + ".", end=" ")
+        else:
+            print(pigLatin(word), end=" ")
+    print("")
+
+
+choice = "y"
+
+print("This program translates words/sentances into pig latin.")
+
+while "y" in choice.lower():
+    word = input("Please enter a word/sentance: ")
+    pigTranslate(word)
+    choice = input("Would you like to enter another word or sentance (y/n)? ")
 
 print("yebay-yebay!")
