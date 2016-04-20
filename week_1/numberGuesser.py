0f80a0f73b5da284c5350b10d589364dfca35123
James Trbovich <jim.trbovich@gmail.com>
2016-04-20 16:40:18 -0500

    added 5 chances for user input

--- week_1/numberGuesser.py
+++ week_1/numberGuesser.py
@@ -4,13 +4,20 @@ def numberGenerator(a = 1, b = 1000):
     return random.randint(a, b)
 
 randomNum = numberGenerator()
+count = 5
 
-print("Guess a number between 1 and 1000")
-userGuess = int(input("Enter your guess: "))
+print("Guess a number between 1 and 1000. You have 5 chances!")
 
-if userGuess == randomNum:
-    print("Correct")
-elif userGuess > randomNum:
-    print("Too high!")
-else:
-    print("Too low!")
+while count:
+    userGuess = int(input("Enter guess #" + str(count) + ": "))
+
+    if userGuess == randomNum:
+        print("Correct")
+    elif userGuess > randomNum:
+        print("Too high!")
+    else:
+        print("Too low!")
+
+    count -= 1
+
+print("Thanks for playing.")
