b9a24817db22be74aa5373b34449858ffe08406f
James Trbovich <jim.trbovich@gmail.com>
2016-04-20 16:19:49 -0500

    added user input to guess a number

--- week_1/numberGuesser.py
+++ week_1/numberGuesser.py
@@ -3,5 +3,12 @@ import random
 def numberGenerator(a = 1, b = 1000):
     return random.randint(a, b)
 
-print(numberGenerator())
-print(numberGenerator(1,10))
+randomNum = numberGenerator()
+
+print("Guess a number between 1 and 1000")
+userGuess = input("Enter your guess: ")
+
+if userGuess == randomNum:
+    print("Correct")
+else:
+    print("Incorrect")
