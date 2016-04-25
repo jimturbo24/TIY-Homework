a40dc217d046f272b196e7d647aad1a557bb0e9a
jimturbo24 <jim.trbovich@gmail.com>
2016-04-24 23:01:19 -0500

    added pep8 comments and updated mysteryWord with pep8 formatting

--- week_1/mysteryWord.py
+++ week_1/mysteryWord.py
@@ -1,37 +1,41 @@
 
-def correctLetterAppend(aList, aChar, aWord):
+def correctLetter_append(aList, aChar, aWord):
     """Adds a letter to a list at the same index position as a
-    letter a word, if the letters match"""
-    letterCount = 0
-    for letter in aWord:
+    letter in a word, if the letters match
+    """
+    #letterCount = 0
+    for letterCount, letter in enumerate(aWord):
         if letter.lower() == aChar.lower() or letter == "'":
             aList[letterCount] = letter
-        letterCount +=1
+        #letterCount +=1
     return aList
 
-def randomWord():
-    """Generates a word at random from a file"""
+
+def random_word():
+    """Generates a word at random from a given file"""
     import random
     f = open("/usr/share/dict/words", "r")
     words = f.readlines()
     f.close
     return words[random.randint(0, len(words))].strip()
 
-def lengthSelector(num=2):
+
+def length_selector(num=2):
     """Selects a word based upon its length"""
-    word = randomWord()
+    word = random_word()
     if num == 1:
         while len(word) >= 6:
-            word = randomWord()
+            word = random_word()
         return word
     elif num == 2:
         while len(word) <= 6 or len(word) >= 10:
-            word =randomWord()
+            word =random_word()
         return word
     elif num == 3:
         while len(word) <= 10:
-            word = randomWord()
+            word = random_word()
         return word
+
 
 playAgain = "y"
 
@@ -41,7 +45,7 @@ If the letters are in the word, the computer (aka. Vanna) will reveal
 where they are in the word. You can guess up to 8 letters per game. You
 will only loose a guess if a letter is not in the word. Have fun!\n""")
 
-while "y" in playAgain.lower(): #Outer game loop
+while "y" in playAgain.lower():  # Outer game loop
 
     diffLevel = ""
 
@@ -50,34 +54,34 @@ while "y" in playAgain.lower(): #Outer game loop
         diffLevel = input("> ")
 
     trys = 8
-    word = lengthSelector(int(diffLevel))
+    word = length_selector(int(diffLevel))
     displayList = (["_"] * len(word))
     guessList = []
 
-    if "'" in word: #Check to remove comma from total letter count
+    if "'" in word:  # Check to remove comma from total letter count
         print("Your mystery word has {0} letters in it. Good Luck!".format(len(word)-1))
     else:
         print("Your mystery word has {0} letters in it. Good Luck!".format(len(word)))
 
-    while trys: #Inner game loop
+    while trys:  # Inner game loop
         userGuess = input("You have {0} trys left. Enter a letter: ".format(trys))
-        if userGuess in guessList:  #Check for previous letter entry
+        if userGuess in guessList:  # Check for previous letter entry
             print("You guessed that letter already!")
             continue
-        if len(userGuess) > 1 or userGuess.isdigit():   #Check for word or number entry
+        if len(userGuess) > 1 or userGuess.isdigit():  # Check for word or number entry
             print("Sorry, no numbers or words please!")
             continue
-        correctLetterAppend(displayList, userGuess, word)
-        if userGuess not in word:   #Decriment player chances if letter not in word
+        correctLetter_append(displayList, userGuess, word)
+        if userGuess not in word:  # Decriment player chances if letter not in word
             print("The letter {0} is not in the mystery word.".format(userGuess))
             trys -= 1
-        if "_" not in displayList:  #Winning condition
+        if "_" not in displayList:  # Winning condition
             print("\n{0} is the word. You won!".format(word))
             break
-        if trys == 0:   #Loosing condition
+        if trys == 0:  # Loosing condition
             print("\nYou're all out of guesses. Your word was {0}".format(word))
             break
-        for char in displayList:    #Print the word with unguessed letters hidden
+        for char in displayList:
             print(char, end=" ")
         print("")
         guessList.append(userGuess)
---
+++ week_1/pep8.md
@@ -0,0 +1,26 @@
+The PEP8 document seemed to be a straightforward attempt at standardizing Python
+code for the diverse Python programming community. Most topics appeared to apply
+to all Python programmers, with the only exception applying to non-English
+speakers ("...please write your comments in English, unless you are 120% sure
+that the code will never be read by people who don't speak your language."). It
+makes sense to try and standardize how code is written, as it will allow for
+code to be more easily implemented on different systems at different times. The
+one caveat to this would be if the standard changes dramatically over what is
+perceived to be a short period of time. Hopefully, PEP8 will be with us for the
+next decade or more.
+
+There was quite a bit in the document that I didn't understand, and thus was not
+very informative (well, outside of the notion that I was informed about that of
+which I do not know). However, there was quite a bit that I found helpful. With
+reference to spaces, I had been using the tab key to indent, but I discovered
+that, "Spaces are the preferred indentation method." Also, I had been using
+spaces throughout operations with multiple operators, but discovered that spaces should only go around the operators with the lowest priority (eg. c = (a+b) * (a-b).
+I also learned that function names should use an underscore to separate words in
+the function name (and have updated my mysteryWord functions as such).
+
+There was a thing or two that struck me as odd. For instance, the naming
+conventions seemed to be very broad (I believe it used the phrase "a bit of a
+mess"). I would certainly vote for removing naming styles such as
+"UPPER_CASE_WITH_UNDERSCORES" or "Capitalized_Words_With_Underscores", as they
+are unpleasant to look at. Also, "Comments should be complete sentences." I know
+I'm going to have trouble with this one...
