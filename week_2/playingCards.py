d2613012cda96e767df568e41644ac990341a2bd
jimturbo24 <jim.trbovich@gmail.com>
2016-04-27 00:32:31 -0500

    added functions and unit tests to 'playingCards' and created the 'beyond PEP8' doc

--- week_2/playingCards.py
+++ week_2/playingCards.py
@@ -1,26 +1,50 @@
+import unittest
+
 class PlayingCard:
     def __init__(self, suit, value):
         if not suit and not value:
             raise AttributeError("Suit and/or value omitted")
-        if suit not in self.suits: #['spades', 'clubs', 'hearts', 'diamonds']:
+        if suit not in self.suitsList:
             raise AttributeError("The card suit entered is invalid")
-        if value not in self.values: #['ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king']
+        if value not in self.valuesList:
             raise AttributeError("The card value entered is invalid")
         self.suit = suit
         self.value = value
 
-    suits = ['spades', 'clubs', 'hearts', 'diamonds']
-    values = ['ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king']
+    suitsList = ['spades', 'clubs', 'hearts', 'diamonds']
+    valuesList = ['ace', '2', '3', '4', '5', '6', '7',
+                  '8', '9', '10', 'jack', 'queen', 'king']
+    valuesDict = {'ace': 'ace', '2': 'two', '3': 'three',
+                  '4': 'four', '5': 'five', '6': 'six',
+                  '7': 'seven', '8': 'eight', '9': 'nine',
+                  '10': 'ten', 'jack': 'jack', 'queen': 'queen',
+                  'king': 'king'}
 
     def shortName(self):
-        pass
+        name = ("{0}{1}".format(self.value[0].capitalize(),
+                                self.suit[0].capitalize()))
+        return name
 
     def longName(self):
-        pass
+        name = ("{0} of {1}".format(PlayingCard.valuesDict
+                                    [self.value].capitalize(),
+                                     self.suit.capitalize()))
+        return name
 
 
-AS = PlayingCard('spades', 'ace')
-AS.suits
+aCard = PlayingCard('clubs', '7')
+# print(aCard.suit, aCard.value)
+# print(PlayingCard.valuesDict)
+# print(aCard.shortName())
+# print(aCard.longName())
 
-print(AS.suit, AS.value)
-print(PlayingCard.suits)
+class TestPlayingCardMethods(unittest.TestCase):
+
+    def test_shortName(self):
+        self.assertEqual(aCard.shortName(), '7C')
+
+    def test_longName(self):
+        self.assertEqual(aCard.longName(), 'Seven of Clubs')
+
+if __name__ == '__main__':
+    unittest.main()
--- week_2/sandbox.py
+++ week_2/sandbox.py
@@ -1,18 +1,26 @@
-class Student:
-    def __init__(self, firstName=None, lastName=None, GPA=None):
-        self.firstName = firstName
-        self.lastName = lastName
-        self.GPA = GPA
+# class Student:
+#     def __init__(self, firstName=None, lastName=None, GPA=None):
+#         self.firstName = firstName
+#         self.lastName = lastName
+#         self.GPA = GPA
+#
+#     def isComplete(self):
+#         return bool(self.firstName and
+#                     self.lastName and
+#                     self.GPA is not None)
+#
+# students = [Student(), Student('jim', 'turbo', 0.0), Student('bob', 'handy', 3.0)]
+# for item in students:
+#     print(item.firstName)
+#     print(item.lastName)
+#     print(item.GPA)
+#     print(item.isComplete())
+#     print("")
 
-    def isComplete(self):
-        return bool(self.firstName and
-                    self.lastName and
-                    self.GPA is not None)
+valuesDict = {'ace': 'ace', '2': 'two', '3': 'three',
+              '4': 'four', '5': 'five', '6': 'six',
+              '7': 'seven', '8': 'eight', '9': 'nine',
+              '10': 'ten', 'jack': 'jack', 'queen': 'queen',
+              'king': 'king'}
 
-students = [Student(), Student('jim', 'turbo', 0.0), Student('bob', 'handy', 3.0)]
-for item in students:
-    print(item.firstName)
-    print(item.lastName)
-    print(item.GPA)
-    print(item.isComplete())
-    print("")
+print(type(valuesDict))
