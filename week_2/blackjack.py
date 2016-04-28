ef608bd2b6a9f766f6af61bb76ca62aa96854e63
James Trbovich <jim.trbovich@gmail.com>
2016-04-28 16:31:38 -0500

    added the hand class and outlined crude game logic

--- week_2/blackjack.py
+++ week_2/blackjack.py
@@ -32,6 +32,14 @@ class PlayingCard:
     def long_name(self):
         return '{0} of {1}'.format(self.values[self.value].capitalize(),
                                    self.suit.capitalize())
+    def num_val(self):
+        if self.value in ('jack', 'queen', 'king'):
+            return 10
+        elif self.value == 'ace':
+            return 11
+        else:
+            return int(self.value)
+
 
 class Deck():
     def __init__(self):
@@ -47,25 +55,75 @@ class Deck():
     def deal_one(self):
         return self.cards.pop()
 
-# card = PlayingCard('5', 'spades')
-# print(type(card))
-#
-# test_deck = [PlayingCard('2', 'clubs'), PlayingCard('7', 'hearts')]
-# print(test_deck[0].value)
+
+class Hand():
+    def __init__(self):
+        self.cards = []
+
+    def dealt_a_card(self, aCard):
+        return self.cards.append(aCard)
+
+    def get_hand(self):
+        for num in range(0, len(self.cards)-1):
+            print(self.cards[num].long_name())
+
+    def sum_of_hand(self):
+        score = 0
+        for num in range(0, len(self.cards)-1):
+            score += self.cards[num].num_val()
+        return score
+
+    def get_results(self):
+        self.get_hand()
+        print('Score: {0}'.format(self.sum_of_hand()))
+
+
 deck = Deck()
+deck.shuffle()
+playerHand = Hand()
+computerHand = Hand()
+playerChoice = 'h'
+
+while len(computerHand.cards) < 3:
+    playerHand.dealt_a_card(deck.deal_one())
+    computerHand.dealt_a_card(deck.deal_one())
+
+while 'h' in playerChoice.lower():
+    print('Your hand: ')
+    playerHand.get_results()
+
+    print('\nComputer hand: ')
+    computerHand.get_results()
+
+    if playerHand.sum_of_hand() == 21:
+        print("You hit 21, you win!")
+        break
+    elif playerHand.sum_of_hand() > 21:
+        print("Sorry, you busted. You loose")
+        break
+    elif computerHand.sum_of_hand() == 21:
+        print("You loose! The computer hit 21")
+        break
+    elif computerHand.sum_of_hand() > 21:
+        print("The computer busted. You win!")
+        break
+    playerChoice = input("\nWould you like to hit(h) or stay(s)? ")
+    if 'h' in playerChoice:
+        playerHand.dealt_a_card(deck.deal_one())
+        if computerHand.sum_of_hand() < playerHand.sum_of_hand():
+            computerHand.dealt_a_card(deck.deal_one())
+    else:
+        if computerHand.sum_of_hand() < playerHand.sum_of_hand():
+            computerHand.dealt_a_card(deck.deal_one())
+
+if computerHand.sum_of_hand() < playerHand.sum_of_hand():
+    print("You beat the computer!")
+elif computerHand.sum_of_hand() > playerHand.sum_of_hand():
+    print("The computer beat you!")
+
+
 # for card in range(0, 52):
 #     print(deck.cards[card].longName())
-deck.shuffle()
-playerHand = []
-playerHand.append(deck.deal_one())
-print(len(playerHand))
-print(len(deck.cards))
-print(playerHand[0].long_name())
-playerHand.append(deck.deal_one())
-print(playerHand[1].long_name())
-
-# print(deck.deal_one().long_name())
-# print(len(deck.cards))
 
 
 # playAgain = "y"
